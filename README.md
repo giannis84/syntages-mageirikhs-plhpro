3e54387b41230036ff0ff1c339f356b30348aa9e
Giannis Kostaras <g_kostaras@msn.com>
2025-03-05 19:53:36 +0200

    Add buttons in app

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # syntages-mageirikhs-plhpro
 
 1. Θα φτιάξουμε στο UI ένα μενού με 4 επιλογές: Αναζήτηση συνταγής, καταχώρηση, τροποποίηση, εκτέλεση.
+    (Σχετικό documentation: https://docs.python.org/3/library/tkinter.html#a-hello-world-program)
--- recipes.py
+++ recipes.py
@@ -5,6 +5,19 @@ root = Tk()
 root.title("New")
 frm = ttk.Frame(root, padding=30)
 frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+ttk.Label(frm, text="Search Recipes").grid(column=0, row=0)
+ttk.Button(frm, text="Search").grid(column=1, row=0)
+
+ttk.Label(frm, text="Save Recipe").grid(column=0, row=1)
+ttk.Button(frm, text="Save").grid(column=1, row=1)
+
+ttk.Label(frm, text="Modify Recipe").grid(column=0, row=2)
+ttk.Button(frm, text="Modify").grid(column=1, row=2)
+
+ttk.Label(frm, text="Execute Recipe").grid(column=0, row=3)
+ttk.Button(frm, text="Execute").grid(column=1, row=3)
+
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=4)
+
 root.mainloop()
