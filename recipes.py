c06cce7ecb4d2b45874d7ab9ebead4ff585e24fb
Giannis Kostaras <g_kostaras@msn.com>
2025-03-21 21:07:12 +0200

    Better alignment in submit recipes window

--- recipes.py
+++ recipes.py
@@ -1,32 +1,38 @@
 import tkinter as tk
 from tkinter import ttk
+import uuid
 
 # Test data for development and a class for the recipes
 class Recipe:
-    def __init__(self, name, category, difficulty, time, ingredients, steps):
-        self.name = name
-        self.category = category
-        self.difficulty = difficulty
-        self.time = time
-        self.ingredients = ingredients
-        self.steps = steps
+	def __init__(self, name="", category="", difficulty="", time="", ingredients="", steps=""):
+		'''Αρχικοποίση της συνταγής'''
+		self.id = uuid.uuid4() # Μοναδικό πρωτεύον κλειδί για χρήση στην βάση δεδομένων
+		self.name = name
+		self.category = category
+		self.difficulty = difficulty
+		self.time = time
+		self.ingredients = ingredients
+		self.steps = steps
 
 
 def filter_by_category(recipes, category):
-    filtered = []
-    for i in recipes:
-        if i.category == category:
-            filtered.append(i)
-    return filtered
+	'''Η συνάρτηση αυτή δεν είναι ακόμη έτοιμη'''
+	filtered = []
+	for i in recipes:
+		if i.category == category:
+			filtered.append(i)
+	return filtered
 
-recipes = []
+recipes = [] # Λίστα όπου θα τοποθετηθούν αντικείμενα της κλάσης "Recipe"
 
+# Παράδειγμα συνταγής:
 Spaghetti_meatballs = Recipe(name="Μακαρόνια με κιμά", category="Ζυμαρικά", difficulty="", time="", ingredients="", steps="")
 
 
 # Main Function
 def main():
-	#ftiaximo para8rou
+	# Πρώτη προσπάθεια να φτιάξουμε κώδικα δημιουργίας παραθύρων
+	# Χρησιμοποιήσαμε κλάσεις που κληρονομούν από την κλάση tk.Toplevel
 	class Search(tk.Toplevel):
 		def __init__(self):
 			super().__init__()
@@ -68,7 +74,8 @@ def main():
 			ttk.Button(self, text = '4').pack()
 			ttk.Label(self, text = 'another label').pack(expand = True)
 
-
+	# Συναρτήσεις που δημιουργούν αντικείμενα των κλάσεων των παραθύρων που θέλουμε να δημιουργήσουμε
+	# Δεν καλούνται άμεσα αλλά ορίζονται ως δείκτες "command" μέσα σε αντικείμενα του module ttk
 	def create_window_Search():
 		global extra_window
 		extra_window = Search()
@@ -86,42 +93,46 @@ def main():
 		extra_window = Execute()
 		
 
+	# Δημιουργία κυρίως παραθύρου
 	root = tk.Tk()
 	root.title("Recipes")
 	frm = ttk.Frame(root, padding=30)
 	frm.grid()
 
-	#buttons
-
-	ttk.Label(frm, text="Search Recipes").grid(column=0, row=0)
-	ttk.Button(frm, text="Search",command = create_window_Search).grid(column=1, row=0)
-	# .pack(expand = True)
-
-
-
-	# Δευτερεύοντα παράθυρα
-
+	# Δευτερεύοντα παράθυρα μπορούν να δημιουργηθούν απευθείας με συναρτήσεις αντί να ορίζουμε κλάσεις όπως πιο πριν
 	def window_submit():
+			'''Συνάρτηση ορισμού του παραθύρου "Submit recipe"'''
+			def save_text():
+				'''Εντολή αποθήκευσης σε αντικείμενο της κλάσης Recipe'''
+				new_recipe = Recipe()
+				name = entry1.get()
+				category = entry2.get()
+				new_recipe = Recipe(name=name, category=category)
+				print(f"Saved new recipe: {new_recipe.name} with id: {new_recipe.id}")
 
-		def save_text():
-			user_input = entry.get()  # Get the text from entry
-			print(f"Saving recipe: {user_input}")
-
-		new_window = tk.Toplevel(root)
-		new_window.title("Submit Recipe")
-		new_window.geometry("300x150")
-		
-		label = tk.Label(new_window, text="Name:")
-		label.pack(pady=5)
-		
-		entry = tk.Entry(new_window)
-		entry.pack(pady=5)
-
-		save_button = tk.Button(new_window, text="Save", command=save_text)
-		save_button.pack(pady=5)
-		
-		close_button = tk.Button(new_window, text="Close", command=new_window.destroy)
-		close_button.pack(pady=5)
+			this_window = tk.Toplevel(root) # Κατασκευή Toplevel widget που έχει ως parent το root widget (κυρίως παράθυρο)
+			this_window.title("Submit recipe details")
+			this_window.geometry("400x200")
+			
+			frame1 = tk.Frame(this_window)
+			frame1.pack(pady=5)
+			label1 = tk.Label(frame1, text="Recipe name:")
+			label1.pack(side=tk.LEFT, padx=8)
+			entry1 = tk.Entry(frame1)
+			entry1.pack(side=tk.RIGHT)
+			
+			frame2 = tk.Frame(this_window)
+			frame2.pack(pady=5)
+			label2 = tk.Label(frame2, text="Difficulty:")
+			label2.pack(side=tk.LEFT, padx=20)
+			entry2 = tk.Entry(frame2)
+			entry2.pack(side=tk.RIGHT)
+			
+			save_button = tk.Button(this_window, text="Save", command=save_text)
+			save_button.pack(pady=15)
+			
+			close_button = tk.Button(this_window, text="Close", command=this_window.destroy)
+			close_button.pack(pady=0)
 
 
 	# Κύριο παράθυρο
